8169897fab4a912a5761bc3cd97ceab767b9b79b
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-07-24 14:06:31 -0700

    Modified _.some to add functionality

--- src/underbar.js
+++ src/underbar.js
@@ -208,6 +208,13 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+
+    return !_.every(collection, function(item) {
+      return !Boolean(iterator(item));
+    });
   };
 
 
