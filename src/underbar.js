17c60e4d1eb4dcc03fa519a042e8c2fa17911f28
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-07-24 14:01:41 -0700

    Modified _.every to add functionality

--- src/underbar.js
+++ src/underbar.js
@@ -192,6 +192,16 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+
+    return _.reduce(collection, function(passedTest, item) {
+      if (!passedTest) {
+        return false;
+      }
+      return Boolean(iterator(item));
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
