b2aed4713fe1b3a9bfecdcf19e8fb694813fb132
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-07-25 11:57:58 -0700

    Modified _.invoke to add functionality

--- src/underbar.js
+++ src/underbar.js
@@ -361,6 +361,15 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    if (typeof functionOrKey === 'function') {
+      return _.map(collection, function(item) {
+        return functionOrKey.apply(item, args);
+      });
+    } else {
+      return _.map(collection, function(item) {
+        return item[functionOrKey].apply(item, args);
+      });
+    }
   };
 
   // Sort the object's values by a criterion produced by an iterator.
